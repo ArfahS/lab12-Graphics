d14e01ff06648ec62611a3148b8e21f697869b1b
Arfah Shamsi <arfah@ou.edu>
2018-11-15 15:33:16 -0600

    Initial commit

---
+++ Drawable.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+
+/**
+ * @author Arfah
+ *
+ */
+public interface Drawable
+{
+public void draw (Graphics g) {
+    
+}
+
+}
---
+++ Shape.java
@@ -0,0 +1,33 @@
+import java.awt.Color;
+import java.awt.Point;
+
+/**
+ * @author Arfah
+ *
+ */
+public class Shape
+{
+    protected Point[] location;
+    private Color color;
+    private boolean filled;
+
+    public Shape(Color color, boolean filled)
+    {
+    }
+
+    public Color getColor()
+    {
+        return null;
+    }
+
+    public boolean isFilled()
+    {
+        return true;
+    }
+
+    public Point[] getLocation()
+    {
+        return null;
+    }
+
+}
